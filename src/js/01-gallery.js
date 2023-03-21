6a52325ce46075285e0c4b6c68702f24032ba934
Dmytro Tkachuk <9demetrius9@gmail.com>
2023-03-21 15:32:26 +0200

    vimeo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galeryEl = document.querySelector(".gallery");
+//створюємо розмітку при завантаженні сторінки
+const markup = galleryItems.map(({ original, preview, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+    </li>`
+).join('');
+//додаємо розмітку до цільового елементу DOM
+galeryEl.insertAdjacentHTML('beforeend', markup);
+//створюємо галерею за допомогою бібліотеки
+const gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250});
 
+//цей рядок був з самого початку
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const VIDEO_SAVED_TIME = 'videoplayer-current-time';
+
+player.on('play', function () {
+    const savedTime = localStorage.getItem(VIDEO_SAVED_TIME)
+    player.setCurrentTime(savedTime).then(function(seconds) {
+        seconds = savedTime;
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                console.log('the time was less than 0 or greater than the video’s duration');
+                break;
+            default:
+                console.log(error.name);
+                break;
+        }
+    });
+});
+    
+player.on('timeupdate', function() {
+
+    player.getCurrentTime().then(_throttle(function(seconds) {
+        localStorage.setItem(VIDEO_SAVED_TIME, seconds);
+        }).catch(function(error) {
+        console.log(error.name);
+    }), 1000);
+    
+});
