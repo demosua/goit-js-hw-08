b584042e0d35efd15079ac1ccb0de42c69f9bf42
Dmytro Tkachuk <68943595+demosua@users.noreply.github.com>
2023-03-21 20:56:18 +0200

    task-2-complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,19 @@
 import Player from '@vimeo/player';
-import _throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const VIDEO_SAVED_TIME = 'videoplayer-current-time';
 
 player.on('play', function () {
-    const savedTime = localStorage.getItem(VIDEO_SAVED_TIME)
+
+    const savedTime = localStorage.getItem(VIDEO_SAVED_TIME);
+
     player.setCurrentTime(savedTime).then(function(seconds) {
         seconds = savedTime;
     }).catch(function(error) {
         switch (error.name) {
             case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
                 console.log('the time was less than 0 or greater than the video’s duration');
                 break;
             default:
@@ -22,12 +23,12 @@ player.on('play', function () {
     });
 });
     
-player.on('timeupdate', function() {
+player.on('timeupdate', throttle(function() {
 
-    player.getCurrentTime().then(_throttle(function(seconds) {
+    player.getCurrentTime().then(function(seconds) {
         localStorage.setItem(VIDEO_SAVED_TIME, seconds);
-        }).catch(function(error) {
+    }).catch(function(error) {
         console.log(error.name);
-    }), 1000);
+    });
     
-});
+}, 1000));
