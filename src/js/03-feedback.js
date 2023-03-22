107ea13bb591db6f2be8184e49bbc75bdd9f4f5c
Dmytro Tkachuk <9demetrius9@gmail.com>
2023-03-22 12:20:31 +0200

    seventh-build

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,14 @@
 import throttle from 'lodash.throttle';
 
-const refs = {
-    form: document.querySelector(".feedback-form"),
-    email: document.querySelector(".feedback-form input"),
-    textArea: document.querySelector(".feedback-form textarea"),
-};
-
+const form = document.querySelector(".feedback-form")
 const SAVED_MESSAGE = 'feedback-form-state';
+
 populateTextArea();
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.email.addEventListener('input', throttle(onEmailInput, 500));
-refs.textArea.addEventListener('input', throttle(onTextAreaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(e => {
+    saveInLocalStorage(e);
+}, 500));
 
 function onFormSubmit(e) {
     e.preventDefault();
@@ -20,30 +17,21 @@ function onFormSubmit(e) {
     localStorage.removeItem(SAVED_MESSAGE);
 }
 
-function onEmailInput(e) {
-    const email = e.target.value;
-    const savedFeedback = JSON.parse(localStorage.getItem(SAVED_MESSAGE));
-    savedFeedback.email = email;
+function saveInLocalStorage(e) {
+    let savedFeedback = JSON.parse(localStorage.getItem(SAVED_MESSAGE));
+    savedFeedback[e.target.name] = e.target.value;
     localStorage.setItem(SAVED_MESSAGE, JSON.stringify(savedFeedback));
 }
-
-function onTextAreaInput(e) {
-    const message = e.target.value;
-    const savedFeedback = JSON.parse(localStorage.getItem(SAVED_MESSAGE));
-    savedFeedback.message = message;
-    localStorage.setItem(SAVED_MESSAGE, JSON.stringify(savedFeedback));
-}
-
 
 function populateTextArea() {
+
     const savedFeedback = JSON.parse(localStorage.getItem(SAVED_MESSAGE));
 
     if(savedFeedback) {
-        refs.email.value = savedFeedback.email;
-        refs.textArea.value = savedFeedback.message;
+        form.querySelector("[name='email']").value = savedFeedback.email;
+        form.querySelector("[name='message']").value = savedFeedback.message;
+        return;
     }
-    else {
-        const savedFeedback = { email: "", message: "" };
-        localStorage.setItem(SAVED_MESSAGE, JSON.stringify(savedFeedback));
-    }
+    
+    localStorage.setItem(SAVED_MESSAGE, JSON.stringify({ email: "", message: "" }));
 }
